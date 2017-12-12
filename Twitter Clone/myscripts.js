841f24a7b575c68a4f6a1f235e6a34acf4663764
Matt Devey <matt.devey@endurance.com>
2017-12-12 13:44:09 -0700

    Cleaned up code and updated styling

--- Twitter Clone/myscripts.js
+++ Twitter Clone/myscripts.js
@@ -7,102 +7,86 @@ var config = {
 };
 firebase.initializeApp(config);
 var chattyref = firebase.database().ref('chatty/');
-function addTweet ()
-{
-  console.log ("writing");
+
+function addTweet() {
   var newTweetRef = chattyref.push();
   var sender = $("#sender").val();
   var message = $("#message").val();
   var timeStamp = new Date();
   if (sender === '')
-      sender = 'Anonymous';
+    sender = 'Anonymous';
   if (message === '')
-      message = '(no message)';
+    message = '(no message)';
   timeStamp = timeStamp.toString();
-  var inReplyTo = null;			// Figure out the best way to do this
-  console.log(sender + ": " + message);
-  console.log(timeStamp);
-  newTweetRef.set ({
+  var inReplyTo = null; // Figure out the best way to do this
+  newTweetRef.set({
     'message': message,
     'sender': sender,
     'timeStamp': timeStamp,
     'inReplyTo': inReplyTo
   });
-  console.log ("written");
 }
-function update_tweetlist()
-{
-    var cnt = 0;
-    $(".tweet").each(function() {
-        cnt++;
-        if (cnt <= cnt_max)
-        {
-          $(this).slideDown();
-        }
-        else
-        {
-          $(this).slideUp();
-        }
-    });
-}
-function startTweetListener ()
-{
-  chattyref.on('child_added', function (tweet) {
-      var message = tweet.val().message;
-      var sender = tweet.val().sender;
-      var key = tweet.key;
-      var time = tweet.val().timeStamp;
-      $("#tweetlist").prepend(
-        '<div id="' + key + '" class="tweet">'
-              + '<button type="button" id="del'+ key + '" class="col-md-1 col-md-offset-11 delete_button btn btn-default"><span class="glyphicon glyphicon-trash"></span></button>'
-              + "Sender: "
-              + sender
-              + "<br>Message: "
-              + message
-              + "<br>Date: "
-              + time +
-        '</div>'
-      );
-      update_tweetlist();
-      //  add handler for adding functionality to the delete button. i.e.
-      $("#del" + key).click(function(){
-        $("#" + key).remove();
-        console.log("pressed delete button");
-        chattyref.child(key).remove();
-        update_tweetlist();
-      });
-  });
-  chattyref.on('child_removed', function(tweet) {
-      $("#" + tweet.key).addClass('redback');
-      $("#" + tweet.key).slideUp(function() {
-          $("#" + tweet.key).remove();
-          update_tweetlist();
-      });
+
+function update_tweetlist() {
+  var cnt = 0;
+  $(".tweet").each(function () {
+    cnt++;
+    if (cnt <= cnt_max) {
+      $(this).slideDown();
+    } else {
+      $(this).slideUp();
+    }
   });
 }
 
-$( document ).ready(function() {
-  // note that the tweetlister may start before the tweet is added
-  // because the random name generator might take some time to respond.
-  startTweetListener ();
-});
-$("document").ready (function () {
-  $(".submitTweet").click (function () {
-		console.log ("clicked");
-		addTweet();
-	});
-});
-$("document").ready (function () {
-  $("#increaseCount").click (function () {
-		cnt_max++;
+function startTweetListener() {
+  chattyref.on('child_added', function (tweet) {
+    var message = tweet.val().message;
+    var sender = tweet.val().sender;
+    var key = tweet.key;
+    var time = tweet.val().timeStamp;
+    $("#tweetlist").prepend(
+      '<div id="' + key + '" class="tweet">' +
+      '<button type="button" id="del' + key + '" class="col-md-1 col-md-offset-11 delete_button btn btn-default"><span class="glyphicon glyphicon-trash"></span></button>' +
+      "Sender: " +
+      sender +
+      "<br>Message: " +
+      message +
+      "<br>Date: " +
+      time +
+      '</div>'
+    );
     update_tweetlist();
-    console.log(cnt_max);
-	});
-});
-$("document").ready (function () {
-  $("#decreaseCount").click (function () {
-		cnt_max--;
+    //  add handler for adding functionality to the delete button. i.e.
+    $("#del" + key).click(function () {
+      $("#" + key).remove();
+      chattyref.child(key).remove();
+      update_tweetlist();
+    });
+  });
+  chattyref.on('child_removed', function (tweet) {
+    $("#" + tweet.key).addClass('redback');
+    $("#" + tweet.key).slideUp(function () {
+      $("#" + tweet.key).remove();
+      update_tweetlist();
+    });
+  });
+}
+
+$(document).ready(function () {
+
+  startTweetListener();
+  $(".submitTweet").click(function () {
+    addTweet();
+  });
+
+  $("#increaseCount").click(function () {
+    cnt_max++;
     update_tweetlist();
-    console.log(cnt_max);
-	});
+  });
+
+  $("#decreaseCount").click(function () {
+    cnt_max--;
+    update_tweetlist();
+  });
 });
